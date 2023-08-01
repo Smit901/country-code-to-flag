772dee83fab217db5ac1ededfd125c309f7ca6f5
Smit901 <smitdudhat10@gmail.com>
2023-08-01 15:34:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+function countryCodeToFlag(countryCode) {
+  if (!countryCode || countryCode.length !== 2) {
+    return 'Invalid country code';
+  }
+
+  const base = 127397; // Unicode offset for regional indicator symbols A
+
+  // Convert country code to uppercase and calculate the flag emoji
+  let flag = '';
+  for (let i = 0; i < countryCode.length; i++) {
+    const codePoint = countryCode.toUpperCase().charCodeAt(i);
+    if (codePoint >= 65 && codePoint <= 90) {
+      // If the character is an uppercase letter (A-Z), add the corresponding regional indicator symbol
+      flag += String.fromCodePoint(codePoint + base);
+    } else {
+      // If the character is not a letter, return "Invalid country code"
+      return 'Invalid country code';
+    }
+  }
+
+  return flag;
+}
+
+module.exports = countryCodeToFlag;
