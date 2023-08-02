df8824e8c4b3b5a44683385bd0fa2d90436ff403
Smit901 <smitdudhat10@gmail.com>
2023-08-02 13:33:17 +0530

    modified

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const twemoji = require('twemoji');
+const twemoji = require("twemoji");
 
 function countryCodeToFlag(countryCode) {
   if (!countryCode || countryCode.length !== 2) {
-    return 'Invalid country code';
+    return "Invalid country code";
   }
 
   const base = 127397; // Unicode offset for regional indicator symbols A
 
   // Convert country code to uppercase and calculate the flag emoji
-  let flag = '';
+  let flag = "";
   for (let i = 0; i < countryCode.length; i++) {
     const codePoint = countryCode.toUpperCase().charCodeAt(i);
     if (codePoint >= 65 && codePoint <= 90) {
@@ -16,11 +16,14 @@ function countryCodeToFlag(countryCode) {
       flag += String.fromCodePoint(codePoint + base);
     } else {
       // If the character is not a letter, return "Invalid country code"
-      return 'Invalid country code';
+      return "Invalid country code";
     }
   }
 
-  return twemoji.parse(flag);
+  return twemoji.parse(flag, {
+    folder: "svg",
+    ext: ".svg",
+  });
 }
 
 module.exports = countryCodeToFlag;
