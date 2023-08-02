5d892ef097e7b917ab3992dd9d2202a833e753c3
Smit901 <smitdudhat10@gmail.com>
2023-08-02 13:37:59 +0530

    1.2

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
-const twemoji = require("twemoji");
+const twemoji = require('twemoji');
 
-function countryCodeToFlag(countryCode) {
-  if (!countryCode || countryCode.length !== 2) {
-    return "Invalid country code";
-  }
-
-  const base = 127397; // Unicode offset for regional indicator symbols A
-
-  // Convert country code to uppercase and calculate the flag emoji
-  let flag = "";
-  for (let i = 0; i < countryCode.length; i++) {
-    const codePoint = countryCode.toUpperCase().charCodeAt(i);
-    if (codePoint >= 65 && codePoint <= 90) {
-      // If the character is an uppercase letter (A-Z), add the corresponding regional indicator symbol
-      flag += String.fromCodePoint(codePoint + base);
-    } else {
-      // If the character is not a letter, return "Invalid country code"
-      return "Invalid country code";
-    }
-  }
-
-  return twemoji.parse(flag, {
-    folder: "svg",
-    ext: ".svg",
+const countryCodeToFlag = (emojiCode) => {
+  const container = document.createElement('div');
+  container.innerHTML = twemoji.parse(emojiCode, {
+    folder: 'svg',
+    ext: '.svg',
   });
-}
+
+  return container.innerHTML;
+};
 
 module.exports = countryCodeToFlag;
